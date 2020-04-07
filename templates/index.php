2150cf27a58f96cf76755993f4e5ddab1f5efe78
Marat <yeffasol@mail.ru>
2020-04-07 09:21:46 +0300

    Add products

--- templates/index.php
+++ templates/index.php
@@ -1,2 +1,71 @@
-<p>main</p>
-<p>new</p>
+<section class="section products">
+    <div class="container container_small">
+        <h2 class="section__title">Новинки</h2>
+        <div class="products__items">
+            <div class="products__item">
+                <div class="products__name">Вахтёр</div>
+                <div class="products__wrapper">
+                    <div class="products__image">
+                        <img src="/images/bottle-left.jpg" alt="Вахтёр">
+                    </div>
+                    <div class="products__desc">
+                        <div class="products__info">
+                            <div class="products__info-item">
+                                <div class="products__info-prop">Алк.</div>
+                                <div class="products__info-value">< 1%</div>
+                            </div>
+                            <div class="products__info-item">
+                                <div class="products__info-prop">Ккал</div>
+                                <div class="products__info-value">35</div>
+                            </div>
+                        </div>
+                        <div class="products__composition">Ржаной хлеб, Вода, Солод, Соль</div>
+                    </div>
+                </div>
+                <div class="products__text">Терпкий и мощный, вкус этого кваса взбодрит после тяжёлого дня и придаст сил
+                    для вечерних приключений
+                </div>
+                <div class="products__bottom">
+                    <div class="products__buttons">
+                        <a href="" class="btn btn_border">Подробнее</a>
+                        <a href="" class="btn btn_primary">Купить</a>
+                    </div>
+                    <div class="products__price">150 Р.</div>
+                </div>
+            </div>
+            <div class="products__item">
+                <div class="products__name">Романтик</div>
+                <div class="products__wrapper">
+                    <div class="products__image">
+                        <img src="/images/bottle-right.jpg" alt="Романтик">
+                    </div>
+                    <div class="products__desc">
+                        <div class="products__desc-inner">
+                            <div class="products__info">
+                                <div class="products__info-item">
+                                    <div class="products__info-prop">Алк.</div>
+                                    <div class="products__info-value">< 0.5%</div>
+                                </div>
+                                <div class="products__info-item">
+                                    <div class="products__info-prop">Ккал</div>
+                                    <div class="products__info-value">27</div>
+                                </div>
+                            </div>
+                            <div class="products__composition">Ржаной хлеб, Вода, Солод, Соль</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="products__text">Терпкий и мощный, вкус этого кваса взбодрит после тяжёлого дня и придаст сил
+                    для вечерних приключений
+                </div>
+                <div class="products__bottom">
+                    <div class="products__buttons">
+                        <a href="" class="btn btn_border">Подробнее</a>
+                        <a href="" class="btn btn_primary">Купить</a>
+                    </div>
+                    <div class="products__price">90 Р.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
